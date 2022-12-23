acae263e96695fd47b452c13b2725efeffde5435
emrah <emrah.eryilmaz@nordeck.net>
2022-12-23 15:17:51 +0300

    refactor: separate config.ts

---
+++ config.ts
@@ -0,0 +1,17 @@
+// keycloak
+export const KEYCLOAK_HOST = "https://ucs-sso-ng.mydomain.corp";
+export const KEYCLOAK_REALM = "jitsi";
+export const KEYCLOAK_CLIENT_ID = "meet";
+
+// jwt
+export const JWT_ALG = "HS256";
+export const JWT_HASH = "SHA-256";
+export const JWT_APP_ID = "myappid";
+export const JWT_APP_SECRET = "myappsecret";
+export const JWT_EXP_SECOND = 3600;
+
+// adapter
+export const HOSTNAME = "0.0.0.0";
+export const PORT = 9000;
+export const PRE = "/oidc";
+export const DEBUG = true;
