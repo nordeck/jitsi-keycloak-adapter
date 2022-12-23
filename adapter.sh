d7b92ed7392fd8c24d9e589fd559d3363fc81e41
emrah <emrah.eryilmaz@nordeck.net>
2022-12-23 15:17:15 +0300

    refactor: prod and testing commands

--- adapter.sh
+++ adapter.sh
@@ -3,4 +3,8 @@ set -e
 
 BASEDIR=$(dirname $0)
 
-deno run --allow-net --unsafely-ignore-certificate-errors $BASEDIR/adapter.ts
+# testing: allow self-signed certificate
+#deno run --allow-net --unsafely-ignore-certificate-errors $BASEDIR/adapter.ts
+
+# prod
+deno run --allow-net $BASEDIR/adapter.ts
