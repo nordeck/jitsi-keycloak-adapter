225e337ef767c05f753ef23ab26eee3b0ed4b9d2
emrah <emrah.eryilmaz@nordeck.net>
2022-12-24 20:34:12 +0300

    refactor: allow self-signed certificate by default

--- adapter.sh
+++ adapter.sh
@@ -4,7 +4,7 @@ set -e
 BASEDIR=$(dirname $0)
 
 # testing: allow self-signed certificate
-#deno run --allow-net --unsafely-ignore-certificate-errors $BASEDIR/adapter.ts
+deno run --allow-net --unsafely-ignore-certificate-errors $BASEDIR/adapter.ts
 
 # prod
-deno run --allow-net $BASEDIR/adapter.ts
+#deno run --allow-net $BASEDIR/adapter.ts
