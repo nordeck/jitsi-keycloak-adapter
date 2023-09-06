54013b0519ee4712c64004da214500b02a0e9ea5
emrah <emrah.eryilmaz@nordeck.net>
2023-09-06 12:46:53 +0300

    refactor: type casting in config.ts

--- config.ts
+++ config.ts
@@ -1,5 +1,6 @@
 // keycloak
-export const KEYCLOAK_ORIGIN = Deno.env.get("KEYCLOAK_ORIGIN") || "https://ucs-sso-ng.mydomain.corp";
+export const KEYCLOAK_ORIGIN = Deno.env.get("KEYCLOAK_ORIGIN") ||
+  "https://ucs-sso-ng.mydomain.corp";
 export const KEYCLOAK_REALM = Deno.env.get("KEYCLOAK_REALM") || "ucs";
 export const KEYCLOAK_CLIENT_ID = Deno.env.get("KEYCLOAK_CLIENT_ID") || "jitsi";
 export const KEYCLOAK_MODE = Deno.env.get("KEYCLOAK_MODE") || "query";
@@ -9,9 +10,9 @@ export const JWT_ALG = Deno.env.get("JWT_ALG") || "HS256";
 export const JWT_HASH = Deno.env.get("JWT_HASH") || "SHA-256";
 export const JWT_APP_ID = Deno.env.get("JWT_APP_ID") || "myappid";
 export const JWT_APP_SECRET = Deno.env.get("JWT_APP_SECRET") || "myappsecret";
-export const JWT_EXP_SECOND = Deno.env.get("JWT_EXP_SECOND") || 3600;
+export const JWT_EXP_SECOND = Number(Deno.env.get("JWT_EXP_SECOND") || 3600);
 
 // adapter
 export const HOSTNAME = Deno.env.get("HOSTNAME") || "127.0.0.1";
-export const PORT = Deno.env.get("PORT") || 9000;
-export const DEBUG = Deno.env.get("DEBUG") || false;
+export const PORT = Number(Deno.env.get("PORT") || 9000);
+export const DEBUG = Deno.env.get("DEBUG") === "true";
