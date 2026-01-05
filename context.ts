328258ce1112c160a33d0e5d765bac9565df0580
emrah <emrah.eryilmaz@conceptboard.com>
2026-01-05 21:41:07 +0300

    refactor: trim values coming from userInfo

--- context.ts
+++ context.ts
@@ -4,13 +4,19 @@
 //
 // Update the codes according to your requirements. Welcome to TypeScript :)
 // -----------------------------------------------------------------------------
+type KeycloakUserInfo = {
+  sub: string;
+  name?: string;
+  preferred_username?: string;
+  email?: string;
+};
 
-export function createContext(userInfo: Record<string, unknown>) {
+export function createContext(userInfo: KeycloakUserInfo) {
   const context = {
     user: {
       id: userInfo.sub,
-      name: userInfo.preferred_username || "",
-      email: userInfo.email || "",
+      name: userInfo.name?.trim() || userInfo.preferred_username?.trim() || "",
+      email: userInfo.email?.trim() || "",
       lobby_bypass: true,
       security_bypass: true,
     },
